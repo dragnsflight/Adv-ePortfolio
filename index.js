9a2352dca55146643621df096fe84b4fa8e7c25c
dragnsflight <marj.furay@gmail.com>
2026-05-12 02:39:48 -0400

    fourth commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// template_3iwhy01
+// service_a9g1952
+// 1sKqvGgp5jiIomTbY
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_a9g1952",
+      "template_3iwhy01",
+      event.target,
+      "1sKqvGgp5jiIomTbY",
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on email@email.com",
+      );
+    });
+}
+
+let isModalOpen = false;
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
